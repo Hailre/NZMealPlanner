adf7613016449fb347442aee5757191b85a61b1f
Hailre <reeceh@stu.otc.school.nz>
2023-09-23 21:52:04 +1200

    Found & retrieved student meal changes from Drive

--- studentmeals.js
+++ studentmeals.js
@@ -1,22 +1,27 @@
 function myFunction() {
   // Declare variables
-  var input, filter, ul, li, a, i, txtValue;
+  var input, filter, ul, li, a, i, txtValue, resultFound;
   input = document.getElementById('searchbox');
   filter = input.value.toUpperCase();
   ul = document.getElementById("studentrecipes");
   li = ul.getElementsByTagName('li');
 
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
+      // Loop through all list items, and hide those who don't match the search query
+   for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            resultFound = true;
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+    if(resultFound) {
+        document.getElementById("search-results").style.display = "";
     } else {
-      li[i].style.display = "none";
+        document.getElementById("search-results").style.display = "block";
     }
   }
-  if (txtValue.toUpperCase().indexOf(filter) != filter){
-    alert("Sorry, we do not have this recipe yet \n Ensure your Recipe is spelt correctly and try again, or email reeceh@stu.otc.school.nz to make a suggestion")
-  }
-}
+
